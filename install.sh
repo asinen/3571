b813529bfb9f89250b1890c2b2e1b68fe529f2c8
Asine <nzxzyh@gmail.com>
2020-05-13 15:41:50 +0800

    no message

---
+++ install.sh
@@ -0,0 +1,23 @@
+cd /root/
+[ -e bityuan ] && cd bityuan && ./bityuan-cli net info && exit 0
+apt-get update
+apt-get install wget -y
+apt-get install supervisor -y
+wget https://raw.githubusercontent.com/asinen/3571/master/bityuan_ubuntu.tar
+tar zxf bityuan_ubuntu.tgz
+cd bityuan
+cp bty.conf /etc/supervisor/conf.d/bty.conf
+supervisorctl reload
+supervisorctl start bty
+sleep 15
+./bityuan-cli net info
+
+cd ~/bityuan
+./bityuan-cli seed generate -l 0  > ~/mySeed.txt
+echo "bty123456" > ~/myPass
+cat ~/mySeed.txt | xargs -I{} ./bityuan-cli seed save -p bty123456 -s "{}"
+./bityuan-cli  wallet unlock  -p bty123456 -s wallet -t 0
+sleep 15
+./bityuan-cli account  list  | grep "airdropaddr" -B 2 | grep addr  -w | cut -d '"' -f 4 > ~/myAirdropAddr
+cat ~/myAirdropAddr | xargs -I{} ./bityuan-cli  account dump_key -a {} > ~/myAirdropPriv
+cat ~/myAirdropPriv | grep data| cut -d '"' -f 4 | xargs  -I{} echo "account import_key -l vul000 -k {} "
